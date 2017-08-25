55ca8cc5f6e431cbc5c3539dca794292b7a1bd85
chase <chase@notebook>
2017-08-25 12:46:47 +0900

    CRUD UPDATE

--- springBootMain/src/test/java/com/chase/SpringBootMainApplicationTests.java
+++ springBootMain/src/test/java/com/chase/SpringBootMainApplicationTests.java
@@ -1,16 +1,30 @@
 package com.chase;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+ 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = SpringBootMainApplicationTests.class)
+@WebAppConfiguration
 public class SpringBootMainApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
+    
+	/*@Autowired
+    private DataSource ds; //작성
+ 
+    @Test
+    public void contextLoads() {
+    }
+ 
+    @Test
+    public void testConnection() throws Exception{ //작성
+        System.out.println("ds : "+ds);
+        
+        Connection con = ds.getConnection(); //ds(DataSource)에서 Connection을 얻어내고
+        
+        System.out.println("con : "+con); //확인 후
+        
+        con.close(); //close
+    }*/
 }
